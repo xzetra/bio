897914c209dc36e82995f089b9a004da3fe07b1b
zetra <105745286+xzetra@users.noreply.github.com>
2024-01-31 16:20:45 -0500

    added site to git

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('themeButton').addEventListener('click' , function(){
+    document.body.classList.toggle('change-theme');
+    paragraph.Style.BorderColor = "black";
+    document.getElementById("theme").style.borderColor = "black";
+});
